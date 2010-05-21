f498419a2924f9d65f9d24743806a71b1d698500
abuchanan <alexbuchanan@abuchanan.local>
2010-05-21 02:11:31 -0700

    Lots of Twitter clean up.  Tests too

--- apps/firefoxcup/views.py
+++ apps/firefoxcup/views.py
@@ -1,27 +1,20 @@
 import jingo
-from bleach import Bleach
-import urllib2
-import json
-import re
-import ttp
-
-bleach = Bleach()
+from twitter import search
+from config import teams, tags
 
 # Create your views here.
 def index(request):
 
-    tweets_json = urllib2.urlopen('http://search.twitter.com/search.json?q=%23io2010')
-    tweets = json.load(tweets_json)
+    tweets = search(tags['all'], request.LANG)
+
+    if (len(tweets) < 15):
+        extra = search(tags['all'], 'all')
+        tweets.extend(extra)
+        
+    # we only want 15 tweets
+    tweets = tweets[:15]
     
-    clean_tweets = []
-    for tweet in tweets['results']:
-        text = tweet['text']
-
-        p = ttp.Parser()
-        text = p.parse(text)
-        cleaned = bleach.clean(text.html, tags=['a'],
-                             attributes={'a': ['href', 'rel']})
-        clean_tweets.append(cleaned)
-
     return jingo.render(request, 'firefoxcup.html', {
-        'tweets': clean_tweets})
+        'tweets': tweets, 
+        'teams': teams})
+
